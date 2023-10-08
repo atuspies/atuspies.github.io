8007486460fd41260f8bb8b437e9f314caa15bde
Melina Carvajal <melina.car13@gmail.com>
2023-10-08 00:17:24 -0400

    Add files via upload

---
+++ modules/footer.php
@@ -0,0 +1,85 @@
+ <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+   
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Links Útiles</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#hero">Inicio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">Nosotros</a></li>
+              <!--li><i class="bx bx-chevron-right"></i> <a href="#services">Servicios</a></li-->
+              <li><i class="bx bx-chevron-right"></i> <a>Terminos de servicios</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a>Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Servicios</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a>Diseño web</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a>Desarrollo web</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a>Pautas Publicitarias</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a>Marketing Digital</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a>Posicionamiento SEO</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contacto</h4>
+            <p>
+              Barrio el troncal <br>
+              Cali, 760001<br>
+              Colombia<br><br>
+              <strong>teléfono:</strong><a href="https://wa.me/+573003500595">+57 300 350 0595<br></a>
+              <strong>Email:</strong> atuspiesdejandohuella@gmail.com<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>Sobre A tus Pies</h3>
+            <p>Ministerio que desea llevar el evangelio a todos los rincones del mundo a través de las herramientas digitales que nos ofrece la tecnología.</p>
+            <div class="social-links mt-3">
+              
+              <a href="https://www.facebook.com/ATusPiesRadio/" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/atuspiesradio/" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://www.youtube.com/@Atuspiesradio" class="youtube"><i class="bx bxl-youtube"></i></a>
+           
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>A tus Pies Radio</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+       
+        Designed by <a href="http://dhovot.com.co/">Dhovot</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+   <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i></a>
+ 
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
