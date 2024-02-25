50edba1766b2d11020f6936b90b02946f980d82e
Melina Carvajal <melina.car13@gmail.com>
2024-02-25 00:05:07 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,2 +1,4 @@
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logo-slider").appendChild(copy);
+var copy = document.querySelector(".logos-slide2").cloneNode(true);
+document.querySelector(".logo-slider2").appendChild(copy);
